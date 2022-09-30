b634c676c2baab4413a06fd482171abbcb911197
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-01 02:26:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,8 @@ import "../styles/App.css";
 const App = () => {
   return (
     <div id="main">
-      <h1 attribute="data-ns-test" value="project-name">
-        Net Banking Portal
-      </h1>
-      <p attribute="data-ns-test" value="project-description">
+      <h1 data-ns-test="project-name">Net Banking Portal</h1>
+      <p data-ns-test="project-description">
         This is Single Page Application made by react.
       </p>
     </div>
