52d4b9757b5621b84bbc9109a87b49b728db92d5
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-01 02:21:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1 attribute="data-ns-test" value="project-name">
+        Net Banking Portal
+      </h1>
+      <p attribute="data-ns-test" value="project-description">
+        This is Single Page Application made by react.
+      </p>
+    </div>
+  );
+};
 
 export default App;
